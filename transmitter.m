514f88b6b5ecd96007f27a64ab1c0abdbd48017d
linzhihui <2935959437@qq.com>
2026-01-18 18:20:11 +0800

    tijiao

--- transmitter.m
+++ transmitter.m
@@ -1,35 +1,37 @@
-function tx_signal = transmitter(bits, N, M, theta, chaos_seq)
-    bits_per_subblock = M + 1;
-    bits_per_symbol = N * bits_per_subblock;
-    
-    num_bits = length(bits);
-    num_symbols = num_bits / bits_per_symbol;
-    bits_reshaped = reshape(bits, bits_per_symbol, num_symbols).';
-    tx_signal = zeros(num_symbols, 2 * theta);
-    
-    for i = 1:num_symbols
-        current_bits = bits_reshaped(i, :);
-        info_bearing_signal = zeros(1, theta);
-        for n = 1:N
-            group_bits = current_bits( (n-1)*bits_per_subblock + 1 : n*bits_per_subblock );
-            map_bits = group_bits(1:M);
-            local_idx = 0;
-            for b = 1:M
-                local_idx = local_idx + map_bits(b) * 2^(M-b); 
-            end
-            % --- 计算全局移位量 ---
-            % 关键修改：必须 +1，使移位范围变为 [1, theta]
-            % 这样接收机检查 k=1 时，才能对应上 local_idx=0
-            block_size = theta / N;
-            global_shift = (n-1) * block_size + local_idx + 1; 
-            
-            mod_bit = group_bits(end);
-            q_n = 2 * mod_bit - 1; 
-            
-            shifted_seq = circshift(chaos_seq, global_shift);
-            info_bearing_signal = info_bearing_signal + q_n * shifted_seq;
-        end
-        tx_signal(i, :) = [chaos_seq, info_bearing_signal];
-    end
+function [Bits,Symbols]=Transmitter(M,N,Block_Num,C)
+    theta=N*(2^M);
+    P=2*(theta+C);%一个完整符号的长度
+    x0=0.123456;
+    Bits=randi([0,1],1,N*(M+1)*Block_Num);
+    Symbols=zeros(P,Block_Num); 
+    for b=1:Block_Num
+        cx0=generate_chaos_seq(theta,x0);
+        cx=(cx0-mean(cx0))/std(cx0);
+        idx_start=(b-1)*N*(M+1)+1;
+        idx_end=b*N*(M+1);
+        bits=Bits(idx_start:idx_end);
+        bits2=reshape(bits,M+1,N);
 
+        info_seq=zeros(1,theta);
+        for n=1:N
+            bits3=bits2(:,n);
+            map_bits=bits3(1:M);
+            mod_bit=bits3(end);
+            local_idx=0;
+            for k=1:M
+                local_idx=local_idx+map_bits(k)*2^(M-k);
+            end
+            zn=(n-1)*(2^M)+local_idx;
+            qn=2*mod_bit-1;
+            cx2=circshift(cx,[0,zn]);
+            info_seq=info_seq+qn*cx2;
+        end
+        ref=[cx(end-C+1:end),cx];
+        info=[info_seq(end-C+1:end),infO_seq];
+        Symbols(:,b)=[ref,info].';
+    end
 end
+            
+
+
+
